6de956d71c9938a931c7d47e4afa2a17894ddaf6
dieKn <dai666k@gmail.com>
2019-04-22 13:21:34 +0000

    prop commit

--- webapp/src/views/Home.vue
+++ webapp/src/views/Home.vue
@@ -1,18 +1,17 @@
 <template>
-  <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js + TypeScript App"/>
-  </div>
+    <div class="home">
+        <MyButton greet="Hello"></MyButton>
+    </div>
 </template>
 
 <script lang="ts">
-import { Component, Vue } from 'vue-property-decorator';
-import HelloWorld from '@/components/HelloWorld.vue'; // @ is an alias to /src
+    import { Component, Vue } from 'vue-property-decorator';
+    import MyButton from "@/components/MyButton.vue";
 
-@Component({
-  components: {
-    HelloWorld,
-  },
-})
-export default class Home extends Vue {}
+    @Component({
+        components: {
+            MyButton,
+    },
+    })
+    export default class Home extends Vue {}
 </script>
