692dc48751f1d9c67e3badc5e114cd570408c9ea
dieKn <dai666k@gmail.com>
2019-04-26 04:40:06 +0000

    input form binding

--- webapp/src/views/Top.vue
+++ webapp/src/views/Top.vue
@@ -2,18 +2,34 @@
   <div class="top">
     <Counter></Counter>
     <Counter setNumber="3"></Counter>
+    <input type="text" name="name" v-model="text">
+    <p>{{ text }}</p>
   </div>
 </template>
 <script lang="ts">
-    import { Component, Vue } from 'vue-property-decorator';
+    import { Component, Emit, Prop, Vue } from 'vue-property-decorator';
     import Counter from "@/components/Counter.vue";
 
     @Component({
         components: {
             Counter,
-    },
+	}
     })
-    export default class Top extends Vue {}
+    export default class Top extends Vue{
+    @Prop() public text!: string;
+
+    @Emit()
+    public input(text: string) {}
+
+    private get localValue(): string {
+      return this.text;
+    }
+
+    private set localValue(text: string) {
+      this.input(text);
+    }
+    }
+ 
 </script>
 <style>
 div.test3{
