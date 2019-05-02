ec72ebbd827e280c709efef66bd484525980d6ef
dieKn <dai666k@gmail.com>
2019-05-02 07:53:21 +0000

    counter function implemented

--- webapp/src/components/Counter.vue
+++ webapp/src/components/Counter.vue
@@ -1,5 +1,8 @@
 <template>
-  <div class="test3">
+  <div id="counter">
+    <div>{{count}}</div>
+    <button v-on:click="decrement">-1</button>
+    <button v-on:click="increment">+1</button>
     <button @click="onClick">Counter</button>
   </div>
 </template>
@@ -9,12 +12,20 @@
 
     @Component
     export default class Counter extends Vue{
+	public name: string = 'Counter';
+	public count: number = 0;
 	@Prop()
 	public setNumber?: number;
-	
+	increment (): void {
+            this.count++;
+        }
+        decrement (): void {
+            this.count--;
+        }	
 	public onClick(){
 		console.log(this.setNumber);
-		alert(this.setNumber + 'aaa');
+		console.log(this.setNumber + 'aaa');
+		alert(this.count);
 		const a: number = 3;
 		console.log(a+2);
 	}
