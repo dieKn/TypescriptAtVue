c343331ffcfb1530cc0e9f4ca1746d54512dc562
DieKn <d372kei@gmail.com>
2019-06-30 07:30:03 +0000

    todo manage function add

--- webapp/src/components/Habit.vue
+++ webapp/src/components/Habit.vue
@@ -1,9 +1,19 @@
 <template>
   <div id="habitList">
-    <div>{{count}}</div>
-    <button v-on:click="increment">-1</button>
-    <button v-on:click="increment">+1</button>
-    <button @click="onClick">Counter</button>
+	<p>
+	    NewHabit:
+	    <input type="text" v-model="newHabit">
+	    <button @click="addHabit">Add</button>
+	  </p>
+	  <hr>
+	<ul>
+	  <li v-for="item in habits">
+	   <input type="checkbox" v-model="item.actCheck" />
+	   <span v-bind:class="{ 'complete': item.actCheck }">{{ item.habit }}</span>
+	   <button v-on:click="deleteHabit(item)">Delete</button>
+	  </li>
+	</ul>
+	<p>Remaining Tasks: {{ remains }}/{{ habits.length }}</p>
   </div>
 </template>
 
@@ -11,23 +21,53 @@
     import {Component, Prop, Vue} from "vue-property-decorator";
 
     @Component
-    export default class Counter extends Vue{
-	public name: string = 'Counter';
-	public count: number = 0;
-	@Prop()
-	public setNumber?: number;
-	increment (): void {
-            this.count++;
-        }
-        decrement (): void {
-            this.count--;
-        }	
-	public onClick(){
-		console.log(this.setNumber);
-		console.log(this.setNumber + 'aaa');
-		alert(this.count);
-		const a: number = 3;
-		console.log(a+2);
+    export default class HabitList extends Vue{
+	newHabit: string = '';
+	habits: {habit: string, actCheck: boolean}[] = [
+			{habit: "Typescript", actCheck: true}, 
+			{habit: "Swift", actCheck: false}
+		];
+	
+	//methods
+	addHabit(): void{
+		if(this.newHabit == ''){ return }
+		this.habits.push(
+			{habit: this.newHabit, actCheck: false}
+		);
+		this.newHabit = '';
+	}
+	deleteHabit(item: {habit: string, actCheck: boolean}): void{
+		//this.habits = this.habits.splice(item, 1);
+		console.log(this.habits.indexOf(item));
+		this.habits.splice(this.habits.indexOf(item), 1);
+	}
+	// computed
+	get remains(): number{
+		let counter: number = 0;
+		for(let i=0; i<this.habits.length; i++ ){
+		  if(!this.habits[i].actCheck){
+		    counter++;
+		  }
+		}
+		return counter;
 	}
     }
+
 </script>
+<style>
+ul {
+  margin: 0;
+  padding: 0;
+  list-style-type: none;
+}
+
+ul > li {
+  margin: 5px;
+  text-indent: 0;
+}
+
+ul > li > .complete {
+  text-decoration: line-through;
+  color: #ddd;
+}
+</style>
