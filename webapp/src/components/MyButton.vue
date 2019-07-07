7986b9f6097fe1132d310f3d4fc19bd440839175
DieKn <d372kei@gmail.com>
2019-07-07 07:00:28 +0000

    axios test

--- webapp/src/components/MyButton.vue
+++ webapp/src/components/MyButton.vue
@@ -1,18 +1,32 @@
 <template>
-    <button @click="onClick">MyButton</button>
+    <button @click="onClickApi">SendButton</button>
 </template>
 
 <script lang="ts">
     import {Component, Prop, Vue} from "vue-property-decorator";
-
+    import axios from "axios";
     @Component
     export default class MyButton extends Vue{
 	@Prop()
 	public greet?: string;
 	
-	public onClick(){
+	public myButton(){
 		alert(this.greet);
 		alert("world!");
+		
+	}
+	public onClickApi(){
+		main().then(res => console.log(res));
 	}
     }
+    
+    export async function main() {
+	  try {
+	    const { data } = await axios.get('http://10.138.97.192:8080/customer');
+	    return data;  // { message: "Real response!" }
+	  } catch (err) {
+	    throw new Error(err.message);
+	  }
+    }
+
 </script>
